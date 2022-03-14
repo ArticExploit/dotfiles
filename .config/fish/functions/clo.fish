5e7e0e647d9fcbe4805b50266cb8c3c42c9b9110
ArticExploit <87346889+ArticExploit@users.noreply.github.com>
2022-03-14 11:00:59 +0100

    Delete .config/fish/functions directory

--- .config/fish/functions/clo.fish
+++
@@ -1,3 +0,0 @@
-function clo --wraps='sudo pacman -Sc && yay -Sc' --description 'alias clo sudo pacman -Sc && yay -Sc'
-  sudo pacman -Sc && yay -Sc $argv; 
-end
--- .config/fish/functions/remove.fish
+++
@@ -1,3 +0,0 @@
-function remove --wraps='sudo pacman -Rns' --description 'alias remove sudo pacman -Rns'
-  sudo pacman -Rns $argv; 
-end
