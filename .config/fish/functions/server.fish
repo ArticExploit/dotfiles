9dfbadedf0ceed5942e6f4c1ecfc9be528ab6d34
ArticExploit <87346889+ArticExploit@users.noreply.github.com>
2022-03-30 13:25:36 +0200

    Delete server.fish

--- .config/fish/functions/server.fish
+++
@@ -1,3 +0,0 @@
-function server --wraps='sudo kitty +kitten ssh -i ~/.ssh/Keys/Server artic@192.168.1.14' --description 'alias server sudo ssh -i ~/.ssh/authorized_keys/id_rsa artic@192.168.1.14'
-  sudo ssh -i ~/.ssh/authorized_keys/id_rsa artic@192.168.1.14 $argv; 
-end
