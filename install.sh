1036c4ed4e6147da3c023e75a8dad459ebf2b734
ArticExploit <87346889+ArticExploit@users.noreply.github.com>
2022-04-05 00:48:03 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
-sudo pacman -Sy nvidia xorg picom i3-wm discord kitty polybar fish rofi flameshot feh ttf-font-awesome noto-fonts-emoji jdk-openjdk gnome-keyring lxappearance btop neofetch 
-yay -Sy brave-bin hamsket sublime-text discordrpcmaker minecraft-launcher breezex-cursor-theme gtk-theme-numix-solarized kora-icon-theme chatterino2-7tv-git deezer picom-ibhagwan-git
+sudo pacman -Sy nvidia xorg picom i3-wm discord kitty polybar fish rofi flameshot feh ttf-font-awesome noto-fonts-emoji jdk-openjdk gnome-keyring lxappearance btop neofetch --noconfirm
+yay -Sy brave-bin hamsket sublime-text discordrpcmaker minecraft-launcher breezex-cursor-theme gtk-theme-numix-solarized kora-icon-theme chatterino2-7tv-git deezer picom-ibhagwan-git --noconfirm
 curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
 omf install yimmy
 set -g fish_greeting ""
